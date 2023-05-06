8db02e4ab88d30d5d3d556dd3af94511fe785bf8
dmytro.babaiev.dev@gmail.com <dmytro.babaiev.dev@gmail.com>
2023-05-06 12:06:20 +0300

    added "multipleMethod"

--- myCalculator/myCalculator.cpp
+++ myCalculator/myCalculator.cpp
@@ -2,14 +2,19 @@
 
 using namespace std;
 
-float plusMethod(float num1, float num2) {
-
+float plusMethod(float num1, float num2) 
+{
     return num1 + num2;
 }
 
-float minusMethod(float num1, float num2) {
-
+float minusMethod(float num1, float num2) 
+{
     return num1 - num2;
+}
+
+float multipleMethod(float num1, float num2)
+{
+    return num1 * num2;
 }
 
 int main()
